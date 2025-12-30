03bb3912ac2aa4ca7c02c509f9651c1f696f76fe
HugoAlejandro2002 <apazahuaychohugoalejandro@gmail.com>
2025-12-30 19:43:59 -0400

    fix: processPaymentValidator

--- src/validators/payment.validator.ts
+++ src/validators/payment.validator.ts
@@ -20,26 +20,6 @@ export const processPaymentValidator = [
     .isString()
     .notEmpty()
     .withMessage('Card token is required'),
-  body('amount')
-    .isFloat({ min: 0.01 })
-    .withMessage('Amount must be greater than 0'),
-  body('currency')
-    .optional()
-    .isIn(['MXN', 'USD'])
-    .withMessage('Currency must be MXN or USD'),
-  body('description')
-    .optional()
-    .isString()
-    .isLength({ max: 255 })
-    .withMessage('Description must be less than 255 characters'),
-  body('customer.email')
-    .optional()
-    .isEmail()
-    .withMessage('Invalid email format'),
-  body('customer.phone')
-    .optional()
-    .isString()
-    .withMessage('Phone must be a string'),
 ];
 
 export const paymentIdParamValidator = [
