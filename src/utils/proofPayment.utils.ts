8a3bb84639f8304b3e20c5248777139d2f556286
Jose Carrasco <jscarrasco18@outlook.com>
2026-04-29 08:27:31 -0400

    feat:add email notification for qr payment (#57)

--- src/utils/proofPayment.utils.ts
+++ src/utils/proofPayment.utils.ts
@@ -5,21 +5,20 @@ import { PaymentType } from '../shared/consts';
 import { formatDateTime } from './dates.util';
 import { sendMail } from './sender.util';
 
-export function sendPaymentProofByEmail(
+export function sendPaymentProofByEmail<T extends ReceiptData>(
   typePayment: PaymentType,
   email: string,
-  payload: Omit<ReceiptData, 'date' | 'time'> &
-    Partial<Pick<ReceiptData, 'date' | 'time'>>,
+  payload: Omit<T, 'date' | 'time'> & Partial<Pick<T, 'date' | 'time'>>,
   extraFilesOptions: AttachmentOption[]
 ): Promise<SendMailResult> {
   const { date, time } = formatDateTime();
-  const data: ReceiptData = {
+  const data = {
     ...payload,
     date: payload.date ?? date,
     time: payload.time ?? time,
     timezone: payload.timezone ?? 'CDMX',
-  };
-  return sendMail<ReceiptData>(
+  } as T;
+  return sendMail<T>(
     getViewProofOfPaymentTemplate(typePayment),
     { to: email, data },
     extraFilesOptions
@@ -31,6 +30,6 @@ export function getViewProofOfPaymentTemplate(
 ): string {
   return path.resolve(
     __dirname,
-    `../../public/emails/${typePayment}-proof-of-payment.email.html`
+    `../../public/emails/proofs-of-payment/${typePayment}-proof-of-payment.email.html`
   );
 }
