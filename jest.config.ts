762e2584ed172e2964beebe493ecf0765036ad5e
Ivan Ticona <ivan.ticona.v@gmail.com>
2026-05-20 13:55:26 -0400

    refactor: move __tests__ folder to project root level

--- jest.config.ts
+++ jest.config.ts
@@ -3,7 +3,7 @@ import type { Config } from 'jest';
 const config: Config = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  roots: ['<rootDir>/src'],
+  roots: ['<rootDir>/__tests__'],
   testMatch: ['**/__tests__/**/*.test.ts'],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
